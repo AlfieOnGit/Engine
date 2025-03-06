4805ff69be609a8d77998633797d4dab5ccb3e3b
Alfie <alfiejay@mail.com>
2025-03-06 13:56:57 +0000

    Restore harmony

--- src/core/Axis.h
+++ src/core/Axis.h
@@ -26,6 +26,7 @@ inline Axis operator--(Axis& axis, int) {
     return axis = static_cast<Axis>((static_cast<int>(axis) - 1 + 4) % 4);
 }
 
+
 inline void print(Axis const& axis) { std::cout << static_cast<int>(axis); }
 
 #endif //AXIS_H
