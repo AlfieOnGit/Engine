e780a800528c7a88dd7fdc9dad78a030c32842c3
AlfieOnGit <alfiejay@mail.com>
2025-04-06 18:08:12 +0200

    Fix matrix multiplication

--- engine/core/Matrix.h
+++ engine/core/Matrix.h
@@ -14,22 +14,38 @@ class Matrix : protected Vector<Vector<T, m>, n>
 public:
     Matrix() = default;
 
+    Matrix(Matrix const& mat) { *this = mat; }
+
+    Matrix& operator=(Matrix const& mat)
+    {
+        for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) this->values[i][j] = mat.values[i][j];
+        return *this;
+    }
+
     [[nodiscard]] Vector<T, m> operator[](int const i) const { return this->values[i]; }
     [[nodiscard]] Vector<T, m>& operator[](int const i) { return this->values[i]; }
 
-    [[nodiscard]] Matrix operator+(Matrix const& target) { return Matrix(this->Vector<Vector<T, m>, n>::operator+(target)); }
-    [[nodiscard]] Matrix operator-(Matrix const& target) { return Matrix(this->Vector<Vector<T, m>, n>::operator-(target)); }
-    [[nodiscard]] Matrix operator*(Matrix const& target) { return Matrix(this->Vector<Vector<T, m>, n>::operator*(target)); }
-    [[nodiscard]] Matrix operator/(Matrix const& target) { return Matrix(this->Vector<Vector<T, m>, n>::operator/(target)); }
+    [[nodiscard]] Matrix operator+(Matrix const& target) const { return Matrix(this->Vector<Vector<T, m>, n>::operator+(target)); }
+    [[nodiscard]] Matrix operator-(Matrix const& target) const { return Matrix(this->Vector<Vector<T, m>, n>::operator-(target)); }
+    [[nodiscard]] Matrix operator*(Matrix const& target) const;
 
     Matrix& operator+=(Matrix const& target) { return *this = *this + target; }
     Matrix& operator-=(Matrix const& target) { return *this = *this - target; }
     Matrix& operator*=(Matrix const& target) { return *this = *this * target; }
-    Matrix& operator/=(Matrix const& target) { return *this = *this / target; }
 
 private:
     explicit Matrix(Vector<Vector<T, m>, n> vec) { for (int i = 0; i < n; i++) this->values[i] = vec[i]; }
 };
+
+
+template<typename T, uint8_t n, uint8_t m>
+Matrix<T, n, m> Matrix<T, n, m>::operator*(Matrix const &target) const
+{
+    Matrix out = Matrix();
+    for (int i = 0; i < n; i++) for (int j = 0; j < m; j++)
+        for (int k = 0; k < n; k++) out.values[i][j] += (*this)[k][j] * target[i][k];
+    return out;
+}
 
 
 namespace Matrices
