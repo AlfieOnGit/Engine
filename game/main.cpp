e7e5533bb9e40da2611cf986a4ca036c151caf5b
AlfieOnGit <alfiejay@mail.com>
2025-04-08 23:53:57 +0200

    Update new renderer

--- game/main.cpp
+++ game/main.cpp
@@ -2,10 +2,11 @@
 
 #include "core/Matrix.h"
 #include "graphics/2D/GraphicsEngine2D.h"
+#include "graphics/3D/Renderer.h"
 
 int main()
 {
-    auto engine = GraphicsEngine2D();
+    auto engine = Renderer();
 
     if (!engine.is_valid())
     {
@@ -13,33 +14,34 @@ int main()
         return -1;
     }
 
-    auto m = Matrices::get_identity<float, 3>();
-    auto m1 = Matrices::get_identity<float, 3>();
-    m1[2][2] = 5.0f;
-    m[1][1] = 2.5f;
-    m += m1;
-    std::cout << "m: \n";
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++) std::cout << m[i][j] << " ";
-        std::cout << '\n';
-    }
-
     // Red triangle
     Mesh* triangle = Mesh::generate_triangle(
         Vector3<float>(-0.5f, -0.5f, 1.0f), // Lower left corner
         Vector3<float>(0.5f, -0.5f, 1.0f), // Lower right corner
         Vector3<float>(0.0f, 0.0f, 1.0f)); // Top middle corner
-    triangle->set_colour(Vector4<float>(1.0f, 0.0f, 0.0f, 0.5f));
-    engine.add_mesh(triangle, 0);
+    triangle->set_colour(Vectors::axis<float, 3>(x) << 0.5f);
+    //triangle->set_colour(Vector4<float>(1.0f, 0.0f, 0.0f, 0.5f));
+
+
+    auto* obj = new GameObject();
+    obj->set_mesh(triangle);
+    obj->get_transform().set_displacement(Vector3<float>(0.5f, 0.0f, 0.0f));
+    obj->get_transform().set_orientation(Quaternion(0,0,-0.38268, 0.92388));
+    engine.add_object(obj);
+
+    auto* obj1 = new GameObject();
+    obj1->set_mesh(triangle);
+    obj1->get_transform().set_displacement(Vector3<float>(-0.5f, 0.0f, 0.0f));
+    obj1->get_transform().set_scale(Vector3<float>(0.5f, 1.0f, 1.0f));
+    engine.add_object(obj1);
 
     // Blue triangle
-    Mesh* triangle2 = Mesh::generate_triangle(
-        Vector3<float>(-0.5f, 0.5f, 0.0f), // Upper left corner
-        Vector3<float>(0.5f, 0.5f, 0.0f), // Upper right corner
-        Vector3<float>(0.0f, -0.5f, 0.0f)); // Lower middle corner
-    triangle2->set_colour(Vector4<float>(0.0f, 0.0f, 1.0f, 1.0f));
-    engine.add_mesh(triangle2, 1);
+    // Mesh* triangle2 = Mesh::generate_triangle(
+    //     Vector3<float>(-0.5f, 0.5f, 0.0f), // Upper left corner
+    //     Vector3<float>(0.5f, 0.5f, 0.0f), // Upper right corner
+    //     Vector3<float>(0.0f, -0.5f, 0.0f)); // Lower middle corner
+    // triangle2->set_colour(Vector4<float>(0.0f, 0.0f, 1.0f, 1.0f));
+    // engine.add_mesh(triangle2, 1);
 
     engine.start();
 
