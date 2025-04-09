945fe9e342a28e6167c9c981aaec721d3fa4e5e8
AlfieOnGit <alfiejay@mail.com>
2025-04-09 19:05:24 +0200

    Patch bug

--- engine/core/Quaternion.h
+++ engine/core/Quaternion.h
@@ -35,7 +35,7 @@ inline Quaternion Quaternion::operator*(Quaternion const &quat) const
         (a[x] * b[w]) + (a[w] * b[x]) + (a[y] * b[z]) - (a[z] * b[y]),
         (a[y] * b[w]) + (a[w] * b[y]) + (a[z] * b[x]) - (a[x] * b[z]),
         (a[z] * b[w]) + (a[w] * b[z]) + (a[x] * b[y]) - (a[y] * b[x]),
-        (a[w] * b[w]) - (a[x] * b[x]) - (a[y] * b[y]) - (a[z] * b[y])
+        (a[w] * b[w]) - (a[x] * b[x]) - (a[y] * b[y]) - (a[z] * b[z])
     };
 }
 
