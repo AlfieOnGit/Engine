e6e7e04400dbd8cc405a9776009fa85252f0bf77
AlfieOnGit <alfiejay@mail.com>
2025-04-09 19:05:56 +0200

    Refactor out old code

--- shaders/3DVert.glsl
+++ shaders/3DVert.glsl
@@ -8,7 +8,5 @@ in vec3 position;
 
 void main() {
     mat4 mvp = projection_matrix * view_matrix * model_matrix;
-    //mat4 mvp = model_matrix * view_matrix * projection_matrix;
-    gl_Position = mvp * vec4(position, 1.0); // Does not work
-    //gl_Position = vec4(position, 1.0); // Works
+    gl_Position = mvp * vec4(position, 1.0);
 }
